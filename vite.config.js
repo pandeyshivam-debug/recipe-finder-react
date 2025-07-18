8d419e6140cc3ecc99ed4822934bdb70fac95d25
pandeyshivam-debug <shivam.pandey@minfytech.com>
2025-07-18 12:37:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,     // Set dev server port
+    host: true      // Allow access from network (e.g., LAN or external IP)
+  }
 })
